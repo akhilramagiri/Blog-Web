06a43d8193d5c87d5c48b37369985466994ec250
Akhil Ramagiri <akhilramagiri11@gmail.com>
2024-04-09 08:37:06 +1000

    Add delete and edit routes

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');    
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -12,13 +17,26 @@ app.use(express.static('public'));
 connectDB();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI
+    }), 
+}));
 //Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine' , 'ejs');
 
-app.use('/', require('./server/route/main'))
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/', require('./server/route/main'));
+app.use('/', require('./server/route/admin'));
 
 app.listen(PORT, ()=> {
     console.log(`listening to port ${PORT}`);
--- server/route/main.js
+++ server/route/main.js
@@ -29,7 +29,8 @@ router.get('', async (req,res) => {
             locals, 
             data,
             current: page,
-            nextPage: hasNextPage? nextPage : null
+            nextPage: hasNextPage? nextPage : null,
+            currentRoute: '/'
         });
 
     }catch (error) {
@@ -54,7 +55,7 @@ router.get('/post/:id',async(req,res) => {
             title: data.title,
             description: "NodeJs"
         }
-        res.render('post',{locals,data});
+        res.render('post',{locals,data, currentRoute: `/post/${slug}`});
     }catch (error) {
         console.log(error);
     }
@@ -88,7 +89,9 @@ router.post('/search', async (req,res) => {
 });
 
 router.get('/about', (req,res) => {
-    res.render('about');
+    res.render('about' , {
+        currentRoute: '/about'
+    });
 })
 
 module.exports = router;
